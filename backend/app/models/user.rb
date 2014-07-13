13b67df5a4170d1a487f30b9214d24f7dc78c6ac
Eric Kelly <heroiceric@gmail.com>
2014-07-12 21:45:16 -0400

    Validate uniqueness of User#uid and User#provider

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true
   validates :provider, presence: true
-  validates :uid, presence: true
+  validates :uid, presence: true, uniqueness: { scope: :provider }
   validates :username, presence: true
 
   def self.find_or_create_from_oauth(oauth)
