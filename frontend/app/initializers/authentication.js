2379c3788be3cc5321d3f42e596c748d8d0a7170
Eric Kelly <heroiceric@gmail.com>
2014-07-13 18:33:52 -0400

    Display current user when signed in

--- frontend/app/initializers/authentication.js
+++ frontend/app/initializers/authentication.js
@@ -1,9 +1,20 @@
+import Ember from 'ember';
 import ToriiOauth2Authenticator from '../authenticators/torii-oauth2';
+import Session from 'simple-auth/session';
 
 export default {
   name: 'authentication',
 
   initialize: function(container) {
+    Session.reopen({
+      currentUser: function() {
+        var userId = this.get('user_id');
+        if (!Ember.isEmpty(userId)) {
+          return container.lookup('store:main').find('user', userId);
+        }
+      }.property()
+    });
+
     container.register('authenticator:torii-oauth2', ToriiOauth2Authenticator);
   }
 };
