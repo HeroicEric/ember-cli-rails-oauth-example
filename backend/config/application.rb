1d62c8903cebcfe10cf89131060d98771bb73959
Eric Kelly <heroiceric@gmail.com>
2014-07-14 13:19:39 -0400

    Allow CORS from firebase deployed app

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -26,5 +26,16 @@ module Example
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
+      allow do
+        origins 'https://ember-cli-rails-auth.firebaseapp.com'
+
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :delete, :put, :options],
+          max_age: 0
+      end
+    end
   end
 end
